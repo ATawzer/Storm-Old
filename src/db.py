3f0f3dfff08960eff45b51f81dfff90f4244d59f
ATawzer <34928044+ATawzer@users.noreply.github.com>
2021-04-27 17:19:22 -0600

    db.py formatting changes (get and update split)

--- src/db.py
+++ src/db.py
@@ -32,6 +32,7 @@ class StormDB:
         self.runs = self.db['runs']
         self.blacklists = self.db['blacklists']
 
+    # Metadata Reading endpoints
     def get_config(self, storm_name):
         """
         returns a storm configuration given its name, assuming it exists.
@@ -69,12 +70,13 @@ class StormDB:
             max_run_idx = np.argmax(np.array([dt.datetime.strptime(x['run_date'], '%Y-%m-%d') for x in r]))
             return r[max_run_idx]
 
+    # Metadata Write Endpoints
     def write_run_record(self, run_record):
 
         q = {}
         self.runs.insert_one(run_record)
 
-    # Playlist
+    # Playlist Reading Endpoints
     def get_playlists(self, name=False):
         """
         Returns all playlist ids in stormdb as a list, or as their names if you'd rather
@@ -133,26 +135,6 @@ class StormDB:
         else:
             raise Exception("Playlist Ambiguous, should be unique to table.")
 
-    def update_playlist(self, pr):
-
-        q = {'_id':pr['_id']}
-
-        # Add new entry or update existing one
-        record = pr
-        changelog_update =  {
-                            'snapshot':pr['info']['snapshot_id'],
-                            'tracks':pr['tracks']
-                            }
-
-        # Update static fields
-        exclude_keys = ['changelog']
-        update_dict = {k: pr[k] for k in set(list(pr.keys())) - set(exclude_keys)}
-        self.playlists.update_one(q, {"$set":record}, upsert=True)
-
-        # Push to append fields (date as new key)
-        for key in exclude_keys:
-            self.playlists.update_one(q, {"$set":{f"{key}.{pr['last_collected']}":changelog_update}}, upsert=True)
-
     def get_loaded_playlist_tracks(self, playlist_id):
         """
         Returns a playlists most recently collected tracks
@@ -179,7 +161,28 @@ class StormDB:
         else:
             return r[0]['artists']
 
-    # Artists
+    # Playlist Write Endpoints
+    def update_playlist(self, playlist_record):
+
+        q = {'_id':playlist_record['_id']}
+
+        # Add new entry or update existing one
+        record = playlist_record
+        changelog_update =  {
+                            'snapshot':playlist_record['info']['snapshot_id'],
+                            'tracks':playlist_record['tracks']
+                            }
+
+        # Update static fields
+        exclude_keys = ['changelog']
+        update_dict = {k: playlist_record[k] for k in set(list(playlist_record.keys())) - set(exclude_keys)}
+        self.playlists.update_one(q, {"$set":record}, upsert=True)
+
+        # Push to append fields (date as new key)
+        for key in exclude_keys:
+            self.playlists.update_one(q, {"$set":{f"{key}.{playlist_record['last_collected']}":changelog_update}}, upsert=True)
+
+    # Artist Reading Endpoints
     def get_known_artist_ids(self):
         """
         Returns all ids from the artists db.
@@ -190,22 +193,6 @@ class StormDB:
         r = list(self.artists.find(q, cols))
 
         return [x['_id'] for x in r]
-
-    def update_artists(self, artist_info):
-        """
-        Updates the artist db with new info
-        """
-
-        for artist in tqdm(artist_info):
-            q = {"_id":artist['id']}
-
-            # Writing updates (formatting changes)
-            artist['last_updated'] = dt.datetime.now().strftime('%Y-%m-%d')
-            artist['total_followers'] = artist['followers']['total']
-            del artist['followers']
-            del artist['id']
-
-            self.artists.update_one(q, {"$set":artist}, upsert=True)
 
     def get_artists_for_album_collection(self, max_date):
         """
@@ -225,24 +212,6 @@ class StormDB:
                 result.append(artist['_id'])
         return result
 
-    def update_artist_album_collected_date(self, artist_ids):
-        """
-        Updates a list of artists album_collected date to today.
-        """
-        date = dt.datetime.now().strftime('%Y-%m-%d')
-
-        for artist_id in tqdm(artist_ids):
-            q = {"_id":artist_id}
-            self.artists.update_one(q, {"$set":{"album_last_collected":date}}, upsert=True)
-
-    def get_blacklist(self, name):
-        """
-        Returns a full blacklist record by name (id)
-        """
-        q = {"_id":name}
-        cols = {"_id":1, "blacklist":1, "type":1, "input_playlist":1}
-        return list(self.blacklists.find(q, cols))
-
     def get_artists_by_genres(self, genres):
         """
         Gets a list artists in DB that have one or more of the genres
@@ -253,6 +222,43 @@ class StormDB:
 
         return [x["_id"] for x in r]
 
+    # Arist Write Endpoints
+    def update_artists(self, artist_info_list):
+        """
+        Updates artist db with list of new artist info
+        """
+
+        for artist in tqdm(artist_info_list):
+            q = {"_id":artist['id']}
+
+            # Writing updates (formatting changes)
+            artist['last_updated'] = dt.datetime.now().strftime('%Y-%m-%d')
+            artist['total_followers'] = artist['followers']['total']
+            del artist['followers']
+            del artist['id']
+
+            self.artists.update_one(q, {"$set":artist}, upsert=True)
+
+    def update_artist_album_collected_date(self, artist_ids, date=None):
+        """
+        Updates a list of artists album_collected date to today by default.
+        """
+        date = dt.datetime.now().strftime('%Y-%m-%d') if date is None else date
+
+        for artist_id in tqdm(artist_ids):
+            q = {"_id":artist_id}
+            self.artists.update_one(q, {"$set":{"album_last_collected":date}}, upsert=True)
+
+    # Blacklist Read Enpoints
+    def get_blacklist(self, name):
+        """
+        Returns a full blacklist record by name (id)
+        """
+        q = {"_id":name}
+        cols = {"_id":1, "blacklist":1, "type":1, "input_playlist":1}
+        return list(self.blacklists.find(q, cols))
+
+    # Blacklist Write Endpoints
     def update_blacklist(self, blacklist_name, artists):
         """
         updates a blacklists artists given its name
@@ -260,21 +266,7 @@ class StormDB:
         q = {"_id":blacklist_name}
         [self.blacklists.update_one(q, {"$addToSet":{"blacklist":x}}) for x in artists]
 
-    # Albums
-    def update_albums(self, album_info):
-        """
-        update album info if needed.
-        """
-
-        for album in tqdm(album_info):
-            q = {"_id":album['id']}
-
-            # Writing updates (formatting changes)
-            album['last_updated'] = dt.datetime.now().strftime('%Y-%m-%d')
-            del album['id']
-
-            self.albums.update_one(q, {"$set":album}, upsert=True)
-
+    # Album Read Endpoints
     def get_albums_by_release_date(self, start_date, end_date):
         """
         Get all albums in date window
@@ -320,13 +312,68 @@ class StormDB:
 
         return [x['_id'] for x in r]
 
-    # Tracks
-    def update_tracks(self, track_info):
+    # Album Write Endpoints
+    def update_albums(self, album_info):
         """
-        update track and its album info if needed.
+        update album info if needed.
         """
 
-        for track in tqdm(track_info):
+        for album in tqdm(album_info):
+            q = {"_id":album['id']}
+
+            # Writing updates (formatting changes)
+            album['last_updated'] = dt.datetime.now().strftime('%Y-%m-%d')
+            del album['id']
+
+            self.albums.update_one(q, {"$set":album}, upsert=True)
+
+    # Track Read Endpoints
+    def get_tracks_for_feature_collection(self):
+        """
+        Get all tracks that need audio features added.
+        """
+        q = {}
+        cols = {"_id":1, "audio_features":1}
+        r = list(self.tracks.find(q, cols))
+
+        # Only append artists who need collection in result
+        result = []
+        for track in r:
+            if 'audio_features' not in track.keys():
+                result.append(track['_id'])
+            else:
+                if not track['audio_features']:
+                    result.append(track['_id'])
+        return result
+
+    def get_tracks_from_albums(self, albums):
+        """
+        returns a track list based on an album list
+        """
+        q = {"album_id":{"$in":albums}}
+        cols = {"_id":1}
+        r = list(self.tracks.find(q, cols))
+
+        return [x["_id"] for x in r]
+
+    def get_track_artists(self, track):
+
+        q = {"_id":track}
+        cols = {"_id":1, "artists":1}
+
+        try:
+            return list(self.tracks.find(q, cols))[0]['artists']
+        except:
+            return [] # not good, for downstream bug fixing
+            raise ValueError(f"Track {track} not found or doesn't have any artists.")
+
+    # Track Write Endpoints
+    def update_tracks(self, track_info_list):
+        """
+        Updates a track and its album frm a list.
+        """
+
+        for track in tqdm(track_info_list):
 
             # Add track to album record
             q = {'_id':track['album_id']}
@@ -352,24 +399,6 @@ class StormDB:
 
             self.tracks.update_one(q, {"$set":track}, upsert=True)
 
-    def get_tracks_for_feature_collection(self):
-        """
-        Get all tracks that need audio features added.
-        """
-        q = {}
-        cols = {"_id":1, "audio_features":1}
-        r = list(self.tracks.find(q, cols))
-
-        # Only append artists who need collection in result
-        result = []
-        for track in r:
-            if 'audio_features' not in track.keys():
-                result.append(track['_id'])
-            else:
-                if not track['audio_features']:
-                    result.append(track['_id'])
-        return result
-
     def update_bad_track_features(self, bad_tracks):
         """
         If tracks that can't get features are identified, mark them here
@@ -384,16 +413,7 @@ class StormDB:
 
             self.tracks.update_one(q, {"$set":track}, upsert=True)
 
-    def get_tracks_from_albums(self, albums):
-        """
-        returns a track list based on an album list
-        """
-        q = {"album_id":{"$in":albums}}
-        cols = {"_id":1}
-        r = list(self.tracks.find(q, cols))
-
-        return [x["_id"] for x in r]
-
+    # DB Cleanup and Prep / other
     def filter_tracks_by_audio_feature(self, tracks, audio_filter):
         """
         Takes in a specific audio_filter format to get tracks with a filter
@@ -404,18 +424,6 @@ class StormDB:
 
         return [x["_id"] for x in r]
 
-    def get_track_artists(self, track):
-
-        q = {"_id":track}
-        cols = {"_id":1, "artists":1}
-
-        try:
-            return list(self.tracks.find(q, cols))[0]['artists']
-        except:
-            return []
-            raise ValueError(f"Track {track} not found or doesn't have any artists.")
-
-    # DB Cleanup and Prep
     def update_artist_albums(self):
         """
         Adds a track list to each artist or appends if not there
