639cb6969575bcabe0b856da5e06426d4c0ab11b
ATawzer <34928044+ATawzer@users.noreply.github.com>
2022-11-18 10:27:19 -0700

    Unsupervised Models
    - Proved out that clustering works on the playlist
    - Created new methods on StormDB and StormUserClient to assist in the clustering

--- test.py
+++ test.py
@@ -9,20 +9,7 @@ from src.runner import *
 from src.analytics import *
 from src.weatherboy import *
 from src.modeling import ModelManager
+from src.storm_client import *
 
-
-ModelManager()
-print("ModelManager Created")
-
-# Test 1
-print("Test 1 - Storm Client Connections and Syntax")
-StormClient(1241528689)
-StormUserClient(1241528689)
-print("Test 1 Success!")
-
-# Test 2
-print("Test 2 - MongoDB Connections and Syntax")
-StormDB()
-print("Test 2 Success!")
-
+print(StormUserClient(1241528689).get_user_playlists())
 
