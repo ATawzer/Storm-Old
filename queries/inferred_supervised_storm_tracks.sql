64ada2c9d518d5606084a38857153367a828440c
ATawzer <34928044+ATawzer@users.noreply.github.com>
2023-04-18 10:52:19 -0600

    Repo cleanup

--- queries/inferred_supervised_storm_tracks.sql
+++
@@ -1,9 +0,0 @@
-select sr.run_id,
-        rh.run_date,
-        ti.*,
-        case when stm.track_id is not null then 1 else 0 end as target_track
-from inferred_storm_run_membership sr
-        left join inferred_run_history rh on sr.run_id = rh.run_id
-        left join track_info ti on sr.track_id = ti._id
-        left join storm_target_membership stm on ti._id = stm.track_id
-                                                and stm.storm_name = rh.storm_name
